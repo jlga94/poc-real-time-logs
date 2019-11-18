b9951a3bf4cca522cc9f120bb3e2bdcbcf4779b7
Jose Aguilar <joseaguilar@LINKIT-MB0229.local>
2019-11-18 10:40:51 +0100

    Kafka producer by parameters

--- log_generator/kafka_producer.py
+++ log_generator/kafka_producer.py
@@ -1,5 +1,6 @@
 import json
 import os
+import argparse
 from time import sleep
 
 from kafka import KafkaConsumer, KafkaProducer
@@ -16,10 +17,10 @@ def publish_message(producer_instance, topic_name, key, value):
         print ('Exception in publishing message')
         print (ex)
 
-def connect_kafka_producer():
+def connect_kafka_producer(kafka_broker):
     _producer = None
     try:
-        _producer = KafkaProducer(bootstrap_servers=['10.15.19.76:9092'], api_version=(0, 10))
+        _producer = KafkaProducer(bootstrap_servers=[kafka_broker], api_version=(0, 10))
     except Exception as ex:
         print ('Exception while connecting Kafka')
         print (ex)
@@ -28,14 +29,18 @@ def connect_kafka_producer():
 
 if __name__ == '__main__':
     topic_name = "http_log"
-    producer = connect_kafka_producer()
+    #kafka_broker = os.getenv('KAFKA_BROKER')
+    kafka_broker = "10.15.19.76:9092"
+    producer = connect_kafka_producer(kafka_broker)
     while(True):
         sleep(5)
         for file in os.listdir("."):
             if file.endswith(".log"):
                 print ("Filename: " + file)
-                with open(file) as f:
+                with open(file,'r') as f:
                     lines = f.readlines()
                     for line in lines:
                         print (line)
                         publish_message(producer, topic_name, 'parsed', line)
+                f = open(file,'w')
+                f.close()
