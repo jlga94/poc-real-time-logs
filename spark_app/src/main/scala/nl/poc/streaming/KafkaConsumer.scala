eaefc8357ee7f78420273c8cf0851d325cab3eb9
Jose Aguilar <joseaguilar@LINKIT-MB0229.local>
2019-11-21 10:36:17 +0100

    Add HTTP Log Class

--- spark_app/src/main/scala/nl/poc/streaming/KafkaConsumer.scala
+++ spark_app/src/main/scala/nl/poc/streaming/KafkaConsumer.scala
@@ -1,10 +1,25 @@
 package nl.poc.streaming
 
+import java.sql.Timestamp
+
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.streaming.Trigger
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming._
 import org.apache.spark.sql.types._
+
+case class SparkApacheLog(host: String,
+                          hyphen: String,
+                          user_id: String,
+                          datetime: Timestamp,
+                          method: String,
+                          resource: String,
+                          protocol: String,
+                          status: String,
+                          size: String,
+                          url: String,
+                          user_agent: String
+                         )
 
 object KafkaConsumer {
   def main(args: Array[String]): Unit = {
@@ -32,6 +47,12 @@ object KafkaConsumer {
     val df = ds
       .selectExpr("CAST(value AS STRING)")
       .as[String]
+
+    case class MyLog ()
+    df.map(string => MyLog)
+    s = string.split(" ")
+    MyLog.firstname = s(0)
+    Mylog.lastname = s(1)
 
     val messagesDF = df.writeStream
       .option("checkpointLocation",args(1))
