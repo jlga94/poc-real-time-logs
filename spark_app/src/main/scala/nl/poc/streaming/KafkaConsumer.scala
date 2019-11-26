a413b020225ffa6e8757cedb466cdedaa62c308f
Jose Aguilar <joseaguilar@LINKIT-MB0229.local>
2019-11-26 11:31:54 +0100

    Get Geolocalization by ip

--- spark_app/src/main/scala/nl/poc/streaming/KafkaConsumer.scala
+++ spark_app/src/main/scala/nl/poc/streaming/KafkaConsumer.scala
@@ -10,6 +10,7 @@ import org.apache.spark.sql.streaming.Trigger
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming._
 import org.apache.spark.sql.types._
+import com.sanoma.cda.geoip.{IpLocation, MaxMindIpGeo}
 
 case class ApacheLog(host: String,
                      hyphen: String,
@@ -21,10 +22,89 @@ case class ApacheLog(host: String,
                      status: Int,
                      bytes: Int,
                      referrer: String,
-                     user_agent: String
+                     user_agent: String,
+                     countryCode: String,
+                     countryName: String,
+                     region: String,
+                     city: String,
+                     geoPointX: Double,
+                     geoPointY: Double,
+                     postalCode: String,
+                     continent: String,
+                     regionCode: String,
+                     continentCode: String,
+                     timezone: String
                     )
 
+case class dataLocation(
+                       countryCode: String,
+                       countryName: String,
+                       region: String,
+                       city: String,
+                       geoPointX: Double,
+                       geoPointY: Double,
+                       postalCode: String,
+                       continent: String,
+                       regionCode: String,
+                       continentCode: String,
+                       timezone: String
+                       )
+
+
+
 object KafkaConsumer {
+
+  def get_data_location(iplocation:IpLocation): dataLocation = {
+    var data_location: dataLocation = null
+    iplocation match {
+      case null => {
+        data_location = new dataLocation(
+          "None",
+          "None",
+          "None",
+          "None",
+          0.0,
+          0.0,
+          "None",
+          "None",
+          "None",
+          "None",
+          "None"
+        )
+      }
+      case _ => {
+        val geopoint = iplocation.geoPoint.getOrElse(null)
+        var latitude = 0.0
+        var longitude = 0.0
+        geopoint match {
+          case null => {
+            latitude = 0.0
+            longitude = 0.0
+          }
+          case _ => {
+            latitude = geopoint.latitude
+            longitude = geopoint.longitude
+          }
+        }
+
+        data_location = new dataLocation(
+          iplocation.countryCode.getOrElse("None"),
+          iplocation.countryName.getOrElse("None"),
+          iplocation.region.getOrElse("None"),
+          iplocation.city.getOrElse("None"),
+          latitude,
+          longitude,
+          iplocation.postalCode.getOrElse("None"),
+          iplocation.continent.getOrElse("None"),
+          iplocation.regionCode.getOrElse("None"),
+          iplocation.continentCode.getOrElse("None"),
+          iplocation.timezone.getOrElse("None")
+        )
+      }
+    }
+    data_location
+  }
+
   def main(args: Array[String]): Unit = {
 
     /*
@@ -42,6 +122,8 @@ object KafkaConsumer {
     val dateFormat = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss", Locale.ENGLISH)
     val PATTERN = """^(\S+) (\S+) (\S+) \[([\w:/]+\s[+\-]\d{4})\] "(\S+)\s?(\S+)?\s?(\S+)?" (\d{3}|-) (\d+|-)\s?"?([^"]*)"?\s?"?([^"]*)?"?$""".r
 
+    val geo_filename = "/Users/joseaguilar/Documents/Repositories/GeoIP/GeoLite2-City_20191105/GeoLite2-City.mmdb"
+
     import spark.implicits._
 
     val ds = spark.readStream
@@ -57,19 +139,35 @@ object KafkaConsumer {
       .as[String].map(stream => {
           val options = PATTERN.findFirstMatchIn(stream.trim())
           val matched = options.get
+          val geoIp = MaxMindIpGeo(geo_filename, 1000, synchronized = true)
+          val iplocation = geoIp.getLocation(matched.group(1)).getOrElse(null)
+
+          val data_location = get_data_location(iplocation)
+
           ApacheLog(
-            matched.group(1),
-            matched.group(2),
-            matched.group(3),
-            new Timestamp(dateFormat.parse(matched.group(4)).getTime()),
-            matched.group(5),
-            matched.group(6),
-            matched.group(7),
-            matched.group(8).toInt,
-            matched.group(9).toInt,
-            matched.group(10),
-            matched.group(11)
-          )
+                matched.group(1),
+                matched.group(2),
+                matched.group(3),
+                new Timestamp(dateFormat.parse(matched.group(4)).getTime()),
+                matched.group(5),
+                matched.group(6),
+                matched.group(7),
+                matched.group(8).toInt,
+                matched.group(9).toInt,
+                matched.group(10),
+                matched.group(11),
+                data_location.countryCode,
+                data_location.countryName,
+                data_location.region,
+                data_location.city,
+                data_location.geoPointX,
+                data_location.geoPointY,
+                data_location.postalCode,
+                data_location.continent,
+                data_location.regionCode,
+                data_location.continentCode,
+                data_location.timezone
+              )
       })
 
     val query = df.writeStream
